2b37038954b6d534d0a825ee7ce9e415d938285f
Zach <Aftermath1991@gmail.com>
2015-10-24 13:14:46 -0700

    Removed unneeded comments

--- project/app/assets/javascripts/node_data_structure.js
+++ project/app/assets/javascripts/node_data_structure.js
@@ -147,26 +147,3 @@ function NumberNode(value) {
 	}
 }
 
-//Prototype functions that should be usable by every function
-/*ExprNode.prototype.toLatex = function() {
-	console.log(".");
-};
-
-ExprNode.prototype.toEval = function() {
-	console.log("I am evaluating.");
-};
-
-//Use object.getName() to receive the name of the constructor as a string
-Object.prototype.getName = function() {  									//Taken from http://stackoverflow.com/questions/332422/how-do-i-get-the-name-of-an-objects-type-in-javascript
-   var funcNameRegex = /function (.{1,})\(/;
-   var results = (funcNameRegex).exec((this).constructor.toString());
-   return (results && results.length > 1) ? results[1] : "";
-};
-*/
-
-//Uneeded due to js being a weakly typed language
-// Prototypes to inherit ExprNode 
-/*EquationNode.prototype = new ExprNode();
-OpNode.prototype = new ExprNode();
-ParensNode.prototype = new ExprNode();
-FunctionNode.prototype = new ExprNode(); */
