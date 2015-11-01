f8f73a86a6a66c1baa4a0a8a74df838014d56a72
Kenny <kemilee@ucsc.edu>
2015-10-31 22:54:33 -0700

    experimented with css buttons and equation view

--- project/app/assets/javascripts/scratchpad.js
+++ project/app/assets/javascripts/scratchpad.js
@@ -1,6 +1,6 @@
 function display_equation() {
     console.log("testing1");
-    $("#article").text("\\( "+parser.parse($("#myEquation").val()).toLatex()+" \\)");
+    $("#article").append("\\( "+parser.parse($("#myEquation").val()).toLatex()+" \\)<br>");
     MathJax.Hub.Queue(["Typeset",MathJax.Hub,document.getElementById('article')]);
     console.log("testing2", $("#article").text());
 }
