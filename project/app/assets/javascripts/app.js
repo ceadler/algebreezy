837138691d897ee06a1e65181b1c6733d6fe09fb
Zach <Aftermath1991@gmail.com>
2015-11-06 11:53:21 -0800

    Enter key press AND mousedown both finally work with display equation in AngularJS

--- project/app/assets/javascripts/app.js
+++ project/app/assets/javascripts/app.js
@@ -2,12 +2,20 @@ angular.module('Algebreezy', [])
 .controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!';
 
   $scope.display_equation = function() {
   	console.log("testing1");
     $("#scratchpad").text("\\( "+parser.parse($("#myEquation").val()).toLatex()+" \\)");
     MathJax.Hub.Queue(["Typeset",MathJax.Hub,document.getElementById('scratchpad')]);
     console.log("testing2", $("#scratchpad").text());
+  }
+
+  $scope.displayEqnKey = function($event) {
+  	if ($event.keyCode === 13) {
+  		display_equation();
+  	}
+  	else {
+  		angular.noop;
+  	}
   }
 }]);
