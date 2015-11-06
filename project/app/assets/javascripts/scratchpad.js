90c1e704a0a66663b675279e6ce9db3c00ab3a3c
Kenny <kemilee@ucsc.edu>
2015-11-06 13:55:28 -0800

    now we have an equation view and tree view!

--- project/app/assets/javascripts/scratchpad.js
+++ project/app/assets/javascripts/scratchpad.js
@@ -5,9 +5,9 @@ function($scope){
 
   $scope.display_equation = function() {
   	console.log("testing1");
-    $("#article").append("\\( "+parser.parse($("#myEquation").val()).toLatex()+" \\)<br>");
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub,document.getElementById('article')]);
-    console.log("testing2", $("#article").text());
+    $("#equation_view").append("\\( "+parser.parse($("#myEquation").val()).toLatex()+" \\)<br>");
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub,document.getElementById('equation_view')]);
+    console.log("testing2", $("#equation_view").text());
   }
 
   $scope.displayEqnKey = function($event) {
